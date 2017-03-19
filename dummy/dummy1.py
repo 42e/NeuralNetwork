993cd199fa5617c19149434bb2f827de356a45e9
David <daudus@volny.cz>
2017-03-19 17:48:48 +0100

    init

---
+++ dummy/dummy1.py
@@ -0,0 +1,17 @@
+'''
+Created on 19. 3. 2017
+
+@author: Daudus
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
